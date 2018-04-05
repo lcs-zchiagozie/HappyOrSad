c28b73f3afc265437ac26b1867be29304ee1b87e
McEnery Peter <pmcenery@lcs-mba3012.local>
2018-04-05 11:17:47 -0400

    class note complete

--- HappyOrSad/main.swift
+++ HappyOrSad/main.swift
@@ -9,9 +9,40 @@
 import Foundation
 
 // Get the user input
+var happyCount = 0
+var sadCount = 0
 var rawInput = readLine()
 
 // Print out the input provided
 print("You said:")
 print(rawInput)
+
+//make sure input is not nil
+// (unwrap the optional)
+guard let input = rawInput else {
+    //If we got here, input is nil, so quit program
+    exit(9) // exit with error code 9
+}
+
+//Print the verified input
+print(input)
+
+//Loop over each character of the input string
+for individualCharacter in input {
+    
+    //debug: what character are we looking at now
+    print(individualCharacter)
+    
+    //is the character happy, sad, or neither
+    if individualCharacter == "" {
+        happyCount += 1
+    } else if individualCharacter == "" {
+        sadCount += 1
+        
+    }
+}
+
+// Outpuut
+
+
 
